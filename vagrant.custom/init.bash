031a0f89c3c83c6b5067ed2aafdfec8c930e80d3
Tsupko Igor (May Cat) <maicatus@gmail.com>
2016-10-10 02:28:04 +0300

    Работает. Остались финальные тесты и публикация

--- vagrant.custom/init.bash
+++ vagrant.custom/init.bash
@@ -9,7 +9,6 @@ HOME="/home/bitrix/"
 mysql -uroot < ${HOME}/vagrant.custom/mysql.sql || exit 1
 
 # Установка Битрикс
-# Попытка сделать то, что делает скрипт http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php
 
 # @todo: нужно сделать какой-то выбор типа редакции
 #http://www.1c-bitrix.ru/private/download/
@@ -32,8 +31,14 @@ mv tmp_b/upload www/upload
 rm tmp_b -rf
 rm start_encode_php5.tar.gz -rf
 
+# Спасаем индексную страницу, потому что следующий шаг её прибьёт :(
+cp ${HOME}www/index.php /tmp/index.php
+
 ## Запускаем установку Битрикса. К сожалению - приходится извращаться и запускать по шагам.
 php -f ${SCRIPT_DIR}install.php || exit 1
+
+# Восстанавливаем индексную страницу
+cp /tmp/index.php ${HOME}www/index.php -f
 
 echo "OLALA!"
 
